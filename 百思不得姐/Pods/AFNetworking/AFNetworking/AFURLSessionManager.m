7a5a41d7b2368fc87fd41c86b3ff214f677c3f38
sunhaichao <584098797@qq.com>
2016-06-04 03:22:27 +0800

    完善

--- 百思不得姐/Pods/AFNetworking/AFNetworking/AFURLSessionManager.m
+++ 百思不得姐/Pods/AFNetworking/AFNetworking/AFURLSessionManager.m
@@ -1,7 +1,7 @@
 // AFURLSessionManager.m
 // Copyright (c) 2011–2016 Alamofire Software Foundation ( http://alamofire.org/ )
 //
-// Permission is hereby granted, free of charge, to any person obtaining a cop
+// Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
 // in the Software without restriction, including without limitation the rights
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
