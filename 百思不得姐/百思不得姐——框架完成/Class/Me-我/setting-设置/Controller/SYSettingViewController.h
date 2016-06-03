40a4f608d44ed809d9bf36ca18b3a78ddd4655d3
sunhaichao <584098797@qq.com>
2016-06-04 03:20:00 +0800

    修复

---
+++ 百思不得姐/百思不得姐——框架完成/Class/Me-我/setting-设置/Controller/SYSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SYSettingViewController.h
+//  百思不得姐——框架完成
+//
+//  Created by 码农界四爷__King on 16/5/16.
+//  Copyright © 2016年 码农界四爷__King. All rights reserved.
+//  我的栏目里右上角设置属性
+
+#import <UIKit/UIKit.h>
+
+@interface SYSettingViewController : UIViewController
+
+@end
---
+++ 百思不得姐/百思不得姐——框架完成/Class/Me-我/setting-设置/Controller/SYSettingViewController.m
@@ -0,0 +1,30 @@
+//
+//  SYSettingViewController.m
+//  百思不得姐——框架完成
+//
+//  Created by 码农界四爷__King on 16/5/16.
+//  Copyright © 2016年 码农界四爷__King. All rights reserved.
+//
+
+#import "SYSettingViewController.h"
+
+@interface SYSettingViewController ()
+
+@end
+
+@implementation SYSettingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"设置";
+    self.view.backgroundColor = [UIColor greenColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
